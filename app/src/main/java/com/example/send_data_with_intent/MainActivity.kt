5f1529f58a798d91155cc9a2c321b1678c210f83
SinaSystem47 <sinasystem47@gmail.com>
2020-01-21 14:46:11 +0330

    Send_data_with_intent

---
+++ app/src/main/java/com/example/send_data_with_intent/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.send_data_with_intent
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked !")
+            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+
+            startActivity(intent)
+        }
+
+    }
+
+
+}
